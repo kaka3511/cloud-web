8dc0326bed84f647641714f0889e6d15f4e99e66
kaka3511 <351139863@qq.com>
2018-06-22 00:16:08 +0800

    天气数据前端展示

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,14 @@
   <div id="app">
     <ul class="layui-nav">
         <li class="layui-nav-item">
-          <a href="">天气<span class="layui-badge">9</span></a>
+          <a href="">天气<span class="layui-badge">{{wetherDesc}}</span></a>
         </li>
-        <li class="layui-nav-item">
+         <li class="layui-nav-item">
+          <a href="">温度<span class="layui-badge">{{weatherTemp}}</span></a>
+        </li>
+        <!-- <li class="layui-nav-item">
           <a href="">控制台<span class="layui-badge">9</span></a>
-        </li>
+        </li> -->
         <li class="layui-nav-item">
           <a href="">个人中心<span class="layui-badge-dot"></span></a>
         </li>
@@ -20,9 +23,16 @@
 <script>
 export default {
   name: 'App',
+  data() {
+    return{
+      wetherDesc:'晴',
+      weatherTemp:'30°c'
+    }
+  },
   created:function(){
-     this.$ajax.get("http://localhost/cloud/weather/getWeatherNow?location=wuhan").then(res=>{
-      console.log(res.data);
+     this.$ajax.get("http://139.199.59.97:8081/cloud/weather/getWeatherNow?location=wuhan").then(res=>{
+      this.wetherDesc = JSON.parse(JSON.stringify(res.data))['responseBody']['weatherDescription'];
+      this.weatherTemp = JSON.parse(JSON.stringify(res.data))['responseBody']['weatherTemperature'];
      });
     }
 
