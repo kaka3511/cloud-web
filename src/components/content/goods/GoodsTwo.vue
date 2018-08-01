1a6335f43880d0c4fd2b04b7641e50c996853b42
fuwei <fuwei@iboxpay.com>
2018-08-01 11:21:01 +0800

    上传支持所有类型文件

--- src/components/content/goods/GoodsTwo.vue
+++ src/components/content/goods/GoodsTwo.vue
@@ -4,15 +4,18 @@
     <div style="padding: 15px;">
 
         <div class="layui-upload">
-          <button type="button" class="layui-btn" id="test1">上传图片</button>
+          <button type="button" class="layui-btn" id="test1">上传文件</button>
           <div class="layui-upload-list">
             <img class="layui-upload-img" id="demo1">
             <p id="demoText"></p>
           </div>
         </div>
     </div>
-
+    已上传的图片
     <table class="layui-hide" id="test" lay-filter="test"></table>
+    <hr>
+    已上传的文件
+    <table class="layui-hide" id="test2" lay-filter="test2"></table>
   </div>
 </template>
 
@@ -24,19 +27,43 @@ export default {
 
 
 
-        //表格展示
+        //图片表格展示
         layui.use('table', function(){
         var table = layui.table;
 
           table.render({
             elem: '#test',
             url:'http://139.199.59.97:8081/cloud/file/queryFile',
+            // url:'http://localhost:80/cloud/file/queryFile?fileType=image',
             cols: [[
               {field:'id', width:120, title: 'ID', sort: true},
-              {field:'fileName', width:500, title: '名称'},
+              {field:'fileName', width:700, title: '名称'},
               {field:'fileType', width:200, title: '类型'},
-              {field:'url', width:500, title: '内容',templet:function(d){
+              {field:'url', width:300, title: '内容',templet:function(d){
                 return '<a target="view_window" href="'+d.url+'"><img style="max-width:100%;max-height:100%" src="'+d.url+'">'+'</img></a>';}},
+
+              {field:'updateTime', width:300, title: '更新时间'}
+            ]],
+            page: true,
+            limit:10
+          });
+        });
+
+
+        //文件表格展示
+        layui.use('table', function(){
+        var table = layui.table;
+
+          table.render({
+            elem: '#test2',
+            url:'http://139.199.59.97:8081/cloud/file/queryFile',
+            // url:'http://localhost:80/cloud/file/queryFile',
+            cols: [[
+              {field:'id', width:120, title: 'ID', sort: true},
+              {field:'fileName', width:700, title: '名称'},
+              {field:'fileType', width:200, title: '类型'},
+              {field:'url', width:300, title: '内容',templet:function(d){
+                return '<a target="view_window" href="'+d.url+'">'+'点击查看'+'</a>';}},
 
               {field:'updateTime', width:300, title: '更新时间'}
             ]],
@@ -51,8 +78,10 @@ export default {
 
           //执行实例
           var uploadInst = upload.render({
+            accept: 'file',
             elem: '#test1' //绑定元素
-            ,url: 'http://139.199.59.97:8081/cloud/file/uploadFile' //上传接口
+            // ,url: 'http://139.199.59.97:8081/cloud/file/uploadFile' //上传接口
+            ,url:'http://localhost:80/cloud/file/uploadFile'
             ,done: function(res){
                 layer.msg("上传成功", {icon: 6});
               //上传完毕回调
